76652bf9ae10ce1eb444481a4ca832e2436a10a9
Elena <angara1298@gmail.com>
2020-07-03 14:59:06 +0300

    default slots and slot defaults

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
             <div class="col-xs-12">
                 <app-quote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p slot="content">kfekfnowe</p>
+                    <p>kfekfnowe</p>
                 </app-quote>
 
             </div>
