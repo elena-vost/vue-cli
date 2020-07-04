7c37e3a0768dc25654fb1d9a4188b1a6f46f3597
Elena <angara1298@gmail.com>
2020-07-04 13:08:35 +0300

    custom control

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@
                         <input
                                 type="text"
                                 id="email"
-                                class="form-control" v-model="userData.email">
+                                class="form-control" 
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
