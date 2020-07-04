4dd11e0fc679da1660e91b91d7130c76ef997d40
Elena <angara1298@gmail.com>
2020-07-04 12:44:55 +0300

    saving line breaks

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,8 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -104,7 +105,7 @@
                         <p>Mail: {{ userData.email }}</p>
                         <p>Password: {{ userData.password }}</p>
                         <p>Age: {{ userData.age }}</p>
-                        <p>Message: </p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
                             <li></li>
@@ -127,7 +128,8 @@
                     email: '',
                     password: '',
                     age: 19
-                }
+                },
+                message: 'A new text'
             }
         }
     }
