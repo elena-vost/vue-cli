52feef17e91a38a41582730561511eeb47db3708
Elena <angara1298@gmail.com>
2020-07-08 18:45:59 +0300

    creating the app

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
        <hr>
        <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <component :is="mode"></component>
+                <component :is="mode" @answered="answered($event)" 
+                @confirmed="mode = 'app-question'"></component>
             </div>  
         </div>       
     </div>
@@ -23,6 +24,16 @@
         data() {
             return {
                 mode: 'app-question'
+            }
+        },
+        methods: {
+            answered(isCorrect) {
+                if (isCorrect) {
+                    this.mode = 'app-answer';
+                } else {
+                    this.mode = 'app-question';
+                    alert('Wrong answer, try again!');
+                }
             }
         },
         components: {
