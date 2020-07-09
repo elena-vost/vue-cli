ef6dda6d440fd89cf12ef315722ac3297ead1088
Elena <angara1298@gmail.com>
2020-07-09 18:50:36 +0300

    where the resource come from

--- src/App.vue
+++ src/App.vue
@@ -33,20 +33,23 @@
                 user: {
                     username:'',
                     email: ''
-                }, users: []
+                }, 
+                users: [],
+                resource: {}
             };
         },
         methods: {
             submit() {
-                this.$http.post('', this.user)
-                    .then(response => {
-                        console.log(response)
-                    }, error => {
-                        console.log(error)
-                    });
+                // this.$http.post('data.json', this.user)
+                //     .then(response => {
+                //         console.log(response)
+                //     }, error => {
+                //         console.log(error)
+                //     });
+                this.resource.save({}, this.user);
             },
             fetchData() {
-                this.$http.get('')
+                this.$http.get('data.json')
                 .then(response => {
                     return response.json();
                 })
@@ -58,7 +61,10 @@
                     this.users = resultArray;
                 });
             }
-        }              
+        },
+        created() {
+            this.resource = this.$resource('data.json');
+        }             
     }
 </script>
 
