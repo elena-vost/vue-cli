30d03ff884018734d6b6939b0bcd921bd264b94c
Elena <angara1298@gmail.com>
2020-07-12 18:58:13 +0300

    adding nav pills

---
+++ src/componets/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <ul class="nav nav-pills">
+        <li role="presentation"><a href="#">Home</a></li>
+        <li role="presentation"><a href="#">User</a></li>
+    </ul>    
+</template>
