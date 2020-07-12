1bcd16b2e3cdb278e1600311782049ef676db1db
Elena <angara1298@gmail.com>
2020-07-12 19:31:36 +0300

    importing users files

--- src/componets/user/User.vue
+++ src/componets/user/User.vue
@@ -2,7 +2,6 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <p>Loaded ID: {{ id }} </p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import User from './componets/user/User.vue';
 import Home from './componets/Home.vue';
+import UserStart from './componets/user/UserStart.vue';
+import UserDetail from './componets/user/UserDetail.vue';
+import UserEdit from './componets/user/UserEdit.vue';
 
 export const routes = [
     { path: '', component: Home },
-    { path: '/user', component: User }
+    { path: '/user', component: User, children: [
+        { path: '', component: UserStart }
+    ] }
 ];
