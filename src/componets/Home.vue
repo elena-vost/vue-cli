85a572d7c57d97d5dbbaebd00ce669f6a8446438
Elena <angara1298@gmail.com>
2020-07-12 18:06:16 +0300

    home file

---
+++ src/componets/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+    <hr>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Ratione possimus facilis, ipsa quod autem, vero magni facere 
+            et soluta earum repellat neque numquam perferendis fugit quaerat 
+            quibusdam nisi necessitatibus suscipit.</p>   
+    </div>
+</template>
