0534a6da116a25bb7258ef2791f0dd989f5159eb
Elena <angara1298@gmail.com>
2020-07-12 19:21:35 +0300

    fetching and using route params

--- src/componets/user/User.vue
+++ src/componets/user/User.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <p>Loaded ID: </p>
+        <p>Loaded ID: {{ id }} </p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
