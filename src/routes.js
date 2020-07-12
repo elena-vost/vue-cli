151cf26b82ae565f73fe3b10159b7b49427f8c88
Elena <angara1298@gmail.com>
2020-07-12 18:17:31 +0300

    routes js setting up

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import User from './componets/user/User.vue';
+import Home from './componets/Home.vue';
+
+export const routes = [
+    { path: '', component: Home },
+    { path: '/user', component: User },
+]
